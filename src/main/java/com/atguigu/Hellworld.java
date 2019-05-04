48484da6c1b1d60bfb51d77d248301bbb169d6e9
hlf_glb <hlf_glb@qq.com>
2019-05-04 19:06:55 +0800

    第一个git工程

---
+++ src/main/java/com/atguigu/Hellworld.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class Hellworld {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hellworld.class, args);
+	}
+
+}
